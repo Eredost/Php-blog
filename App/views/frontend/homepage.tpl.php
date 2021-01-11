f9cb4ebdfc34520682bc874f0551f8b21e78855e
Eredost <michael.coutin62@gmail.com>
2021-01-11 15:16:14 +0100

    feat: integrate banner and about sections

--- App/views/frontend/homepage.tpl.php
+++ App/views/frontend/homepage.tpl.php
@@ -1,7 +1,32 @@
 <?php ob_start(); ?>
 
-<p>Hello world !</p>
-<i class="fa fa-user" aria-hidden="true"></i>
+<section class="banner">
+    <div class="section-wrapper">
+        <div class="banner__head">
+            <h1>Michaël COUTIN</h1>
+            <p>A vos côtés pour tous vos projets web</p>
+            <a href="#contact" class="button">Me contacter</a>
+        </div>
+        
+        <div class="banner__picture">
+            <img src="uploads/profile-picture.png" alt="Michaël Coutin">
+        </div>
+    </div>
+</section>
+
+<section class="about">
+    <div class="section-wrapper">
+        <h2 class="section-title">A propos</h2>
+
+        <p>Passionné par le monde du développement, j'ai appris en grande majorité sur mon temps de pause en autodidacte à travers diverses plateformes de cours en ligne et en faisant mes premières armes avec le langage Python avant de me lancer dans le développement web.</p>
+        <p>J'aime sans cesse découvrir des nouvelles technologies et me lancer des challenges.</p>
+
+        <a href="#" class="button">
+            <i class="fa fa-download" aria-hidden="true"></i>
+            Télécharger mon CV
+        </a>
+    </div>
+</section>
 
 <?php
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./scss/main.scss";
 
 let app = {
-    init: function () {
-        let navbarToggle = document.getElementById('navbar-toggle');
-        navbarToggle.addEventListener('click', app.handleNavbarToggle);
+    init() {
+        let navbarToggle = document.getElementById("navbar-toggle");
+        navbarToggle.addEventListener("click", app.handleNavbarToggle);
     },
 
-    handleNavbarToggle: function () {
-        let navbarElement = document.getElementById('navbar');
-        navbarElement.classList.toggle('visible');
+    handleNavbarToggle() {
+        let navbarElement = document.getElementById("navbar");
+        navbarElement.classList.toggle("visible");
     }
-}
+};
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
