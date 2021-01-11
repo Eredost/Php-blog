f2cfc1d2d72b46581cbece984eb1271ca883c2de
Eredost <michael.coutin62@gmail.com>
2021-01-11 15:16:14 +0100

    feat: integrate banner and about sections

--- App/views/layouts/_header.tpl.php
+++ App/views/layouts/_header.tpl.php
@@ -7,7 +7,7 @@
 
     <i id="navbar-toggle" class="fa fa-bars header__navbar-toggle" aria-hidden="true"></i>
 
-    <nav id="navbar" class="navbar visible">
+    <nav id="navbar" class="navbar">
         <ul>
             <li class="navbar__item">
                 <a href="#" class="navbar__link active">Accueil</a>
