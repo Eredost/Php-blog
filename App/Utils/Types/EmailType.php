5d5f18bd3716cf9eedfc23091a6214ce4e03a8be
Eredost <michael.coutin62@gmail.com>
2021-01-16 15:41:15 +0100

    fix: replace single quotes with double quotes in input field classes

--- App/Utils/Types/EmailType.php
+++ App/Utils/Types/EmailType.php
@@ -9,10 +9,10 @@ class EmailType extends Field
         $requiredLabel = $this->getRequired() ? '*' : '';
 
         return "
-            <div class='form__group'>
-                <label for='{$this->getName()}' class='form__label'>{$this->getLabel()} <span class='required'>{$requiredLabel}</span></label>
-                <input type='email' name='{$this->getName()}' id='{$this->getName()}' class='form__input' value='{$this->getValue()}' placeholder='{$this->getPlaceholder()}'>
-                <p class='form__error'>{$this->getErrorMessage()}</p>
+            <div class=\"form__group\">
+                <label for=\"{$this->getName()}\" class=\"form__label\">{$this->getLabel()} <span class=\"required\">{$requiredLabel}</span></label>
+                <input type=\"email\" name=\"{$this->getName()}\" id=\"{$this->getName()}\" class=\"form__input\" value=\"{$this->getValue()}\" placeholder=\"{$this->getPlaceholder()}\">
+                <p class=\"form__error\">{$this->getErrorMessage()}</p>
             </div>
         ";
     }
--- App/Utils/Types/TextAreaType.php
+++ App/Utils/Types/TextAreaType.php
@@ -9,9 +9,9 @@ class TextAreaType extends Field
         $requiredLabel = $this->getRequired() ? '*' : '';
 
         return "
-            <div class='form__group'>
-                <label for='{$this->getName()}' class='form__label'>{$this->getLabel()} <span class='required'>{$requiredLabel}</span></label>
-                <textarea name='{$this->getName()}' id='{$this->getName()}' cols='30' rows='10' class='form__input' placeholder='{$this->getPlaceholder()}'></textarea>
+            <div class=\"form__group\">
+                <label for=\"{$this->getName()}\" class=\"form__label\">{$this->getLabel()} <span class=\"required\">{$requiredLabel}</span></label>
+                <textarea name=\"{$this->getName()}\" id=\"{$this->getName()}\" cols=\"30\" rows=\"10\" class=\"form__input\" placeholder=\"{$this->getPlaceholder()}\"></textarea>
             </div>
         ";
     }
