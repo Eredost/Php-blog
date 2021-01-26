7e2a8f8e93c6ca44c09b304b81cca4ce0061f01b
Eredost <michael.coutin62@gmail.com>
2021-01-26 16:01:41 +0100

    feat: add stylesheets, scripts and fonts generated by Webpack

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ use Blog\Exceptions\HttpException;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+session_set_cookie_params([
+    'httponly' => true,
+]);
 session_start();
 
 try {
